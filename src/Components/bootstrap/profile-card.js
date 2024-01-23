c566bad45a0a06f2ede82bdb0f07c15c3bd8aeeb
tarunreddybabi <tarunreddybabi@gmail.com>
2024-01-23 12:11:53 +0530

    profile-card

--- src/Components/bootstrap/profile-card.js
+++ src/Components/bootstrap/profile-card.js
@@ -3,14 +3,14 @@
 const ProfileCard=(prop)=>{
     return(
         <>
-        <div classNameName="card" style={{width:"360px"}}>
-    <img classNameName="card-img-top" src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt="Card" style={{width:"100%"}} />
-    <div classNameName="card-body">
-      <h4 classNameName="card-title" style={{color:prop.details.color}}>{prop.details.name}</h4>
-      <p classNameName="card-text">{prop.details.designation}</p>
-      <p classNameName="card-text">{prop.details.salary}</p>
+        <div className="card" style={{width:"360px"}}>
+    <img className="card-img-top" src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt="Card" style={{width:"100%"}} />
+    <div className="card-body">
+      <h4 className="card-title" style={{color:prop.details.color}}>{prop.details.name}</h4>
+      <p className="card-text">{prop.details.designation}</p>
+      <p className="card-text">{prop.details.salary}</p>
 
-      <a href="#d" classNameName="btn btn-primary">See Profile</a>
+      <a href="#d" className="btn btn-primary">See Profile</a>
     </div>
   </div>
   </>
