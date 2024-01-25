feb6a27817537b1cfc29fb48c4ea9f23a16a8030
tarunreddybabi <tarunreddybabi@gmail.com>
2024-01-25 07:06:21 +0530

    controlled component react

--- src/Components/controlled/controlled.js
+++ src/Components/controlled/controlled.js
@@ -1,12 +1,57 @@
 import { useState } from "react";
 
 const Controlled = () => {
+  const [userName, setUserName] = useState("");
+  const [userNameError, setUserNameError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-    const[userName,setUserName]=useState("")
-    const userNameHandler=(event)=>{
-        console.log(event.target.value)
-        setUserName(event.target.value)
+  const userNameHandler = (event) => {
+    setUserName(event.target.value);
+    if (event.target.value.length > 10) {
+      setUserNameError("User name should be below 10 characters");
+    } else {
+      setUserNameError("");
     }
+  };
+
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+    const result = validatePassword(event.target.value);
+    if (result) {
+      console.log("password valid");
+      console.log(event.target.value);
+      setPasswordError("");
+    } else {
+      setPasswordError("password invalid");
+      console.log(event.target.value);
+
+    }
+  };
+
+  const username = "john1";
+
+  function validatePassword(psw) {
+    let msg = true;
+
+    switch (true) {
+      case psw.length < 8 || psw.length > 13:
+        msg = false;
+        break;
+      case !/[0-9]/.test(psw):
+        msg = false;
+        break;
+      case psw === psw.toLowerCase() || psw === psw.toUpperCase():
+        msg = false;
+        break;
+      case psw.includes(username):
+        msg = false;
+        break;
+      default:
+        msg = true;
+    }
+    return msg;
+  }
 
   return (
     <>
@@ -21,7 +66,11 @@ const Controlled = () => {
             name="username"
             value={userName}
             onChange={userNameHandler}
+            style={userNameError ? { border: "2px solid red" } : {border:"2px solid green"}}
           />
+          {userNameError ? (
+            <h4 style={{ color: "red" }}>{userNameError}</h4>
+          ) : null}
         </div>
         <div className="mb-3">
           <label className="form-label">Password:</label>
@@ -30,8 +79,17 @@ const Controlled = () => {
             className="form-control"
             id="pwd1"
             placeholder="Enter password"
+            value={password}
+            onChange={passwordHandler}
+            style={passwordError ?{border:"2px solid red"}:{border:"2px solid green"} }
           />
+          {passwordError ? <h4 style={{color:"red"}}>{passwordError}</h4> : null}
         </div>
+        {userNameError || passwordError ? null : (
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        )}
       </form>
     </>
   );
