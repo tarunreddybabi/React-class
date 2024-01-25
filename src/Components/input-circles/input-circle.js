8837db37222d90b8f9b811cb1dc5f59808821f11
tarunreddybabi <tarunreddybabi@gmail.com>
2024-01-25 21:41:32 +0530

    circles task

---
+++ src/Components/input-circles/input-circle.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const InputCircles = () => {
+  const [numberOfCircles, setNumberOfCircles] = useState(0);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const result = parseInt(event.target.elements["add-circle"].value);
+    console.log(result);
+    if (!isNaN(result) && result >= 1 && result <= 50) {
+      setNumberOfCircles(result);
+    } else {
+      alert("Enter values between 1 to 50");
+    }
+  };
+
+  const renderCircles = () => {
+    return [...Array(numberOfCircles)].map((_, index) => (
+      <h2
+        key={index}
+        style={{
+          height: "100px",
+          width: "100px",
+          border: "4px solid indigo",
+          borderRadius: "50%",
+        }}
+      >
+        {" "}
+      </h2>
+    ));
+  };
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <div className="mb-3">
+          <label className="form-label">Enter a number</label>
+          <input
+            type="text"
+            className="form-control"
+            id="add-circle"
+            placeholder="Enter values between 1 to 50"
+            name="add-circle"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          click to add circles
+        </button>
+      </form>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>{renderCircles()}</div>
+    </>
+  );
+};
+export default InputCircles;
