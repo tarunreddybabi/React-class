46472917dacc630dbee43c0c2a9029e5912ce942
tarunreddybabi <tarunreddybabi@gmail.com>
2024-01-31 14:59:02 +0530

    use effect

--- src/Components/hooks/use-effect/useEffect3.js
+++ src/Components/hooks/use-effect/useEffect3.js
@@ -1,31 +1,27 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
+const UseEffect3 = () => {
+  useEffect(() => {
+    addEvent();
+    return () => {
+      removeEvent();
+    };
+  }, []);
+  const addEvent = () => {
+    document.addEventListener("click", clickCapture);
+  };
+  const removeEvent = () => {
+    document.removeEventListener("click", clickCapture);
+  };
 
+  const clickCapture = (event) => {
+    console.log(event);
+  };
 
-const UseEffect3=()=>{
-
-    useEffect(()=>{
-        addEvent()        
-        return ()=>{
-            removeEvent()            
-        }
-
-    },[])
-    const addEvent=()=>{
-        document.addEventListener("click",clickCapture)
-    }
-    const removeEvent=()=>{
-        document.removeEventListener("click",clickCapture)
-    }
-
-    const clickCapture=(event)=>{
-        console.log(event)
-    }
-
-    return(
-        <>
-        <h1>Use Effect-3</h1>
-        </>
-    )
-}
-export default UseEffect3
+  return (
+    <>
+      <h1>Use Effect-3</h1>
+    </>
+  );
+};
+export default UseEffect3;
